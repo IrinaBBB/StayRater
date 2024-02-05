1869edc1926595f5aee55a1297593fe92ff32c90
IrinaB <balkarovairina@gmail.com>
2024-02-05 21:09:24 +0100

    Add Initial StayRaterServer Setup

---
+++ StayRaterServer/src/main/kotlin/ru/aurorahost/Application.kt
@@ -0,0 +1,14 @@
+package ru.aurorahost
+
+import io.ktor.server.application.*
+import ru.aurorahost.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
