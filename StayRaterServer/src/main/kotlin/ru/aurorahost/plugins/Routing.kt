3193e803355e52064adaa79992960c9b2ee9c629
IrinaB <balkarovairina@gmail.com>
2024-02-07 11:11:16 +0100

    Add GetAllHotels Endpoint

--- StayRaterServer/src/main/kotlin/ru/aurorahost/plugins/Routing.kt
+++ StayRaterServer/src/main/kotlin/ru/aurorahost/plugins/Routing.kt
@@ -2,12 +2,14 @@ package ru.aurorahost.plugins
 
 import io.ktor.application.Application
 import io.ktor.routing.routing
+import ru.aurorahost.routes.getAllHotels
 import ru.aurorahost.routes.root
 
 
 fun Application.configureRouting() {
     routing {
         root()
+        getAllHotels()
     }
 }
 
---
+++ StayRaterServer/src/main/kotlin/ru/aurorahost/routes/AllHotels.kt
@@ -0,0 +1,38 @@
+package ru.aurorahost.routes
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import ru.aurorahost.models.ApiResponse
+import ru.aurorahost.repositories.HotelRepository
+import org.koin.ktor.ext.inject
+
+
+fun Route.getAllHotels() {
+    val hotelRepository: HotelRepository by inject()
+
+    get("/hotels") {
+        try {
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+            println("NEW PAGE: $page")
+            require(page in 1..5)
+
+            val apiResponse = hotelRepository.getAllHotels(page = page)
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
+        } catch (e: NumberFormatException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Only Numbers Allowed."),
+                status = HttpStatusCode.BadRequest
+            )
+        } catch (e: IllegalArgumentException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Heroes not Found."),
+                status = HttpStatusCode.NotFound
+            )
+        }
+    }
+}
