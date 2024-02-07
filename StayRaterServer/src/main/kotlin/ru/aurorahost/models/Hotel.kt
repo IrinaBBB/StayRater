c3e540056f7fa08aae60041af15b7dabc84de235
IrinaB <balkarovairina@gmail.com>
2024-02-07 10:29:35 +0100

    Add Hotel and ApiResponse Models (server)

---
+++ StayRaterServer/src/main/kotlin/ru/aurorahost/models/Hotel.kt
@@ -0,0 +1,18 @@
+package ru.aurorahost.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Hotel(
+    val id: Int,
+    val name: String,
+    val location: String,
+    val description: String,
+    val rating: Double,
+    val numberOfRooms: Int,
+    val checkInTime: String,
+    val checkOutTime: String,
+    val amenities: List<String>,
+    val roomTypes: List<String>,
+    val services: List<String>
+)
