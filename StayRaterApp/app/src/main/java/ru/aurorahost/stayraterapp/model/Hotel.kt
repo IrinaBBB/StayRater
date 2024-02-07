cb6bfc6e231130ea828c25bb74d2fac573eca6fc
IrinaB <balkarovairina@gmail.com>
2024-02-07 15:35:27 +0100

    Add Hotel Model

---
+++ StayRaterApp/app/src/main/java/ru/aurorahost/stayraterapp/model/Hotel.kt
@@ -0,0 +1,23 @@
+package ru.aurorahost.stayraterapp.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ru.aurorahost.stayraterapp.util.Constants.HOTEL_DATABASE_TABLE
+
+@Entity(tableName = HOTEL_DATABASE_TABLE)
+data class Hotel(
+
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val name: String,
+    val location: String,
+    val description: String,
+    val rating: Double,
+    val numberOfRooms: Int,
+    val checkInTime: String,
+    val checkOutTime: String,
+    val amenities: List<String>,
+    val roomTypes: List<String>,
+    val services: List<String>,
+    val image: String
+)
--- StayRaterApp/app/src/main/java/ru/aurorahost/stayraterapp/util/Constants.kt
+++ StayRaterApp/app/src/main/java/ru/aurorahost/stayraterapp/util/Constants.kt
@@ -2,4 +2,5 @@ package ru.aurorahost.stayraterapp.util
 
 object Constants {
     const val DETAILS_ARGUMENT_KEY = "hotelId"
+    const val HOTEL_DATABASE_TABLE = "hotel_table"
 }
