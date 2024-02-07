102f75c75a5e2d612653f40ebb02e9fabed86499
IrinaB <balkarovairina@gmail.com>
2024-02-07 12:19:00 +0100

    Add Showing Static Images

--- StayRaterServer/src/main/kotlin/ru/aurorahost/plugins/Routing.kt
+++ StayRaterServer/src/main/kotlin/ru/aurorahost/plugins/Routing.kt
@@ -1,6 +1,8 @@
 package ru.aurorahost.plugins
 
 import io.ktor.application.Application
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
 import io.ktor.routing.routing
 import ru.aurorahost.routes.getAllHotels
 import ru.aurorahost.routes.root
@@ -10,6 +12,10 @@ fun Application.configureRouting() {
     routing {
         root()
         getAllHotels()
+
+        static("/images") {
+            resources("images")
+        }
     }
 }
 
